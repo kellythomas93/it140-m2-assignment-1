5f318bdb6b1de852a6e3f9799f0f577ad2f80827
kellythomas93 <kelly.a.thomas93@gmail.com>
2026-09-16 12:16:00 -0500

    Update name_age.py

--- Part-A/src/name_age.py
+++ Part-A/src/name_age.py
@@ -1,20 +1,21 @@
 """TODO: Replace with a one-line summary of the program's purpose (<73 chars).
 
 Input:
-    TODO: Replace with a major input, including its type and source.
-    TODO: Replace with another major input, or delete this TODO line.
-    TODO: Replace with another major input, or delete this TODO line.
+    
+    user_name = input("What is your name? ")
+    user_age = int(input("How old are you? "))
 
 Process:
-    TODO: Replace with a major processing step.
+    current_year = datetime.datetime.now().year
+  birth_year = current_year - user_age
 
 Output:
-    TODO: Replace with a major output, including its type and destination.
+    print(f"\nHello {user_name}! You were born in {birth_year}.")
 
 Typical usage example:
-    TODO: Replace with the input prompt and original name-input example.
-    TODO: Replace with the input prompt and original age-input example.
-    TODO: Replace with the resulting output from those inputs.
+  What is your name? Alex
+How old are you?24
+    Hello Alex! You were born in 2001.
 """
 
 # === Imports ===
@@ -28,15 +29,12 @@ CURRENT_YEAR = date.today().year  # Get current year from system as integer
 def main() -> None:
     """Run the name-age program."""
 
-    # Get user input.
-    # TODO: Replace with code to get user's name as a string. See zyBooks 1.3.
-    # TODO: Replace with code to get user's age as an integer. See zyBooks 2.6.
+    user_name = input("What is your name? ")
+    user_age = int(input("How old are you? "))
+ current_year = datetime.datetime.now().year
+  birth_year = current_year - user_age
+ print(f"\nHello {user_name}! You were born in {birth_year}.")
 
-    # Calculate user's approximate birth year.
-    # TODO: Replace with code to process data. See zyBooks 1.16 & 1.17.
-
-    # Output personalized message with user's name and birth year.
-    # TODO: Replace with code to output formatted results. zyBooks 1.3 & 2.7.
 
 
 # === Main Guard ===
@@ -45,5 +43,4 @@ if __name__ == "__main__":
 
 
 # === References ===
-# TODO: Replace with an APA-style reference for a source you used, or delete.
-# TODO: Replace with another APA-style reference, or delete this TODO line.
+
